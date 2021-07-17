a70c912506046b891180271f0193a18c7d992671
ArkURL <1035096867@qq.com>
2021-07-17 16:41:53 +0800

    add exportJosn function

--- Arknight_operators_birthdays/pipelines.py
+++ Arknight_operators_birthdays/pipelines.py
@@ -6,8 +6,27 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exporters import JsonItemExporter
 
 
 class ArknightOperatorsBirthdaysPipeline:
+    #
+    def __init__(self):
+        self.file = open('arknights.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, ensure_ascii=False, encoding='utf-8')
+        self.exporter.start_exporting()
+
+    #
+    def open_spider(self, spider):
+        print("spider启动")
+
+    # spider
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+    #
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+        print("spider调用结束")
--- Arknight_operators_birthdays/run.py
+++ Arknight_operators_birthdays/run.py
@@ -1,3 +1,3 @@
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl Ark -o ark.json -t json".split())
+cmdline.execute("scrapy crawl Ark".split())
