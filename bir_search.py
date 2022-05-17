c02c4bae646e72fcc69e11223e05cf2c8f0fb2bb
ArkURL <1035096867@qq.com>
2022-05-17 15:22:53 +0800

    remove some api which were used in GUI version

--- bir_search.py
+++ bir_search.py
@@ -1,5 +1,6 @@
-import pickle
+# import pickle
 import json
+import datetime
 
 """
 with open('data.pik', 'rb') as f:
@@ -7,64 +8,31 @@ with open('data.pik', 'rb') as f:
 """
 
 
-def get_operatos_list():
+def get_operators_list_for_cmdline():
     try:
-        with open('../arknights.json', 'r', encoding='utf-8') as f:
-            operators_list = json.load(f)
-            return operators_list
+        with open('./arknights.json', 'r', encoding='utf-8') as f:
+            return json.load(f)
     except Exception as e:
         print(e)
 
 
-# 图形界面使用
-# 通过查询直接返回结果
-def from_operatos_get_birthdays(operator_name):
-    operators_list = get_operatos_list()
-    for each in operators_list:
-        if each['name'] == operator_name:
-            return each['birthday']
-    return False
-
-
-# 图形界面使用
-# 通过查询返回结果，注意可能有多个返回结果
-def from_date_get_operators(date):
-    operators_list = get_operatos_list()
-    result = []
-    for each in operators_list:
-        if date == each['birthday']:
-            result.append(each['name'])
-    return result
-
-def from_date_get_test():
-    prompt = '按q退出，输入日期查询是否有对应干员在指定日期生日：'
-    while True:
-        birth = input(prompt)
-        if birth == 'q':
-            break
-        result = from_date_get_operators(birth)
-        if len(result) == 0:
-            print('看来没有和你同一天生日的干员呢...')
-        else:
-            for operator in result:
-                print(f'你和{operator}同一天生日哦！')
-
-        prompt = '请输入日期，按q退出：'
-
-
-
-# 通过生日查询ri天生日干员函数
+# 通过日期查询对应生日干员函数
 def birth_to_operators():
-    operators_list = get_operatos_list()
+    operators_list = get_operators_list_for_cmdline()
     prompt = "请输入你的生日(格式为xx月xx日，如12月24日，输入q返回上一级）:"
     while True:
         tag = 0
-        birth = input(prompt)
-        if birth == 'q':
+        user_input = input(prompt)
+        if user_input == 'q':
             break
+        elif user_input == '今天':
+            now = datetime.datetime.now()
+            month = now.month
+            day = now.day
+            user_input = str(month) + '月' + str(day) + '日'
         prompt = "请输入你的生日:"
         for each in operators_list:
-            if birth == each['birthday']:
+            if user_input == each['birthday']:
                 print('你和{0}同一天生日哦！'.format(each['name']))
                 tag = 1
         if tag == 0:
@@ -73,7 +41,7 @@ def birth_to_operators():
 
 # 查询特定干员生日函数
 def operators_to_birth():
-    operators_list = get_operatos_list()
+    operators_list = get_operators_list_for_cmdline()
     prompt = "请输入你要查询的干员（如：能天使，输入q返回上一级）:"
     while True:
         operator = input(prompt)
@@ -83,7 +51,7 @@ def operators_to_birth():
             break
         for each in operators_list:
             if operator == each['name']:
-                print("干员{0}的生日是{1}".format(each['name'],each['birthday']))
+                print("干员{0}的生日是{1}".format(each['name'], each['birthday']))
                 tag = 1
         if tag == 0:
             print('未查询到对应干员，请输入有效的干员名')
@@ -94,5 +62,3 @@ if __name__ == '__main__':
     birth_to_operators()
     print('测试：根据干员名查询生日')
     operators_to_birth()
-    print('图形界面用测试：根据生日查询干员')
-    from_date_get_test()
